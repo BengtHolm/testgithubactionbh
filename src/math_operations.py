d8c08bd30a2ef45bd1cc166dda1d3817d3d8b28c
BengtHolm <bengt.s.holm@gmail.com>
2025-09-20 23:42:18 +0200

    Unit testcases updated

---
+++ src/math_operations.py
@@ -0,0 +1,24 @@
+def add(a, b):
+    """
+    Returns the sum of a and b.
+
+    Args:
+        a (float or int): First number.
+        b (float or int): Second number.
+
+    Returns:
+        float or int: The sum of a and b.
+    """
+    return a + b
+
+def sub(a, b):
+    """
+    Returns the difference of a and b.
+
+    Args:
+        a (float or int): First number.
+        b (float or int): Second number.
+    Returns:
+        float or int: The difference of a and b.    
+    """
+    return a - b
